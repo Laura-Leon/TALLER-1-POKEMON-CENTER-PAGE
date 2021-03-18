1fd2bf9025050d6353244e718fb5efc8a90d67d3
Laura <isabelaleon8@gmail.com>
2021-03-18 18:49:48 -0500

    interaction slider for mobile almost done, interaction for pc, done

--- script/main.js
+++ script/main.js
@@ -28,10 +28,21 @@ const container = document.querySelector(".interaction__container");
 
 const drags =(e)=>{
   let xPos = e.layerX;
+  let size = container.offsetWidth;
   before.style.width = xPos + "px";
+  slider.style.left = xPos + "px";
 
+if(xPos < 50){
+  before.style.width =0;
+  slider.style.left = 0;
+}
 
+if(xPos + 30 >size){
+  before.style.width =size + "px";
+  slider.style.left = size + "px";
+}
 
 };
 
 container.addEventListener("mousemove",drags);
+container.addEventListener("touchmove",drags);
